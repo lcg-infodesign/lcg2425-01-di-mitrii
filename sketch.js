2384194246d9679ae27f956482ef488d85ab0812
di-mitrii <dimi.maioglio@gmail.com>
2024-10-24 22:39:26 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,41 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  createCanvas(400, 500);
+  background(214,209, 202); 
+  noLoop();
 }
 
 function draw() {
-  // put drawing code here
+  let numSquares = 100; 
+  let squareSize = 20; 
+  let paddingy = 10;  
+  let paddingx = 2;
+
+  let gridCols = 10;
+  let gridRows = 10;
+
+  let gridWidth = width / 2;
+  let gridHeight = height / 2;
+
+  let offsetX = width / 4;
+  let offsetY = height / 4;
+
+  let alpha = 175; 
+
+  let numelem = (numSquares / gridRows);
+  let halfnum = numelem / 2;
+
+  for (let i = 0; i < numSquares; i++) {
+    let col = i % gridCols;
+    let row = floor(i / gridCols);
+    let delta = abs(((col < halfnum) ? 0 : numelem - 1) - col);
+    let randy = paddingy + delta;
+
+    let x = offsetX + col * (gridWidth / gridCols) + random(-paddingx, paddingx);
+    let y = offsetY + row * (gridHeight / gridRows) + 10 * delta + random( 0 , randy);
+
+    fill(0, 0, 0, alpha); 
+    noStroke();
+    
+    rect(x, y, squareSize, squareSize);
+  }
 }
